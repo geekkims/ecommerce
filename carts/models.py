a9421be0581d2f26ec2bc0e382fa534851b98526
Techspace <jaytechub@gmail.com>
2023-02-04 14:40:38 +0300

    fixed cat bugs and also worked on cart context processor

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 from store.models import Product
 
 # Create your models here.
--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from carts.models import CartItem
+from carts.views import _cart_id
 from category.models import Category
 
 from store.models import Product
@@ -30,10 +33,12 @@ def store(request,category_slug=None):
 def product_detail(request,category_slug,product_slug):
     try:
         single_product=Product.objects.get(category__slug=category_slug,slug=product_slug)
+        in_cart=CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
     except Exception as e:
         raise e
     context={
-        "single_product":single_product
+        "single_product":single_product,
+        "in_cart":in_cart,
     }
     
 
