7b82bbee3cab56869695c6ee4efc52b521bca6cb
Techspace <jaytechub@gmail.com>
2023-02-05 12:23:27 +0300

    finalized on variations model

--- carts/admin.py
+++ carts/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from carts.models import Cart, CartItem
+from carts.models import Cart, CartItem, Variation
 
 # Register your models here.
+class VariationAdmin(admin.ModelAdmin):
+    list_display=('product','variation_category','variation_value','is_active','created_date')
+    list_editable=('is_active',)
+    list_filter=('product','variation_category')
 
 
 admin.site.register(Cart)
 admin.site.register(CartItem)
+admin.site.register(Variation,VariationAdmin)
--- carts/models.py
+++ carts/models.py
@@ -24,3 +24,21 @@ class CartItem(models.Model):
 
     def __str__(self):
         return self.product
+    
+variation_category_choices=(
+    ('color','color'),
+    ('size','size'),
+  
+)
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    variation_category =models.CharField(max_length=255,choices=variation_category_choices)
+    variation_value=models.CharField(max_length=255)
+    is_active=models.BooleanField(default=True)
+    created_date=models.DateTimeField(auto_now=True)
+
+
+    def __unicode__(self):
+        return self.product
